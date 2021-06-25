fbb38c06db3067a2919b282f1e72a3213c91a40f
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-25 17:16:21 +0200

    Add map class unit tests

--- shm/test/map-ut.cpp
+++ shm/test/map-ut.cpp
@@ -4,13 +4,20 @@
 #include "shm/inc/Map.hpp"
 #undef private
 
-extern size_t ISLANDS_COUNT;
-
 namespace shm_test {
-
+Map map;
 TEST(MapTest, ShouldGenerateVectorOfIslands) {
-    Map map;
     EXPECT_EQ(map.vectorOfIslands_.size(), ISLANDS_COUNT);
+}
+TEST(MapTest, ShouldReturnAllIslands) {
+    for (const auto& island : map.vectorOfIslands_) {
+        auto coords = island.getCoordinates();
+        EXPECT_EQ(map.getIsland(coords), &island);
+    }
+}
+TEST(MapTest, ShouldReturnCurrentPosition) {
+    auto FIRST_ISLAND = &map.vectorOfIslands_[0];
+    EXPECT_EQ(map.currentPosition_, FIRST_ISLAND);
 }
 
 }  // namespace shm_test
