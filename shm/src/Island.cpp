63c06baa60205dfedd40d9fd4e35c59f56449b09
BarTes8 <barbara.tessema@gmail.com>
2021-07-04 12:39:32 +0200

    added distance() function

--- shm/src/Island.cpp
+++ shm/src/Island.cpp
@@ -1,5 +1,7 @@
 #include "shm/inc/Island.hpp"
 
+#include <cmath>
+
 Island::Coordinates::Coordinates(size_t positionX, size_t positionY) 
     : positionX_(positionX)
     , positionY_(positionY)
@@ -7,6 +9,10 @@ Island::Coordinates::Coordinates(size_t positionX, size_t positionY)
 
 bool Island::Coordinates::operator==(const Coordinates& position) const {
     return (positionX_ == position.positionX_ && positionY_ == position.positionY_); 
+}
+
+static size_t Island::Coordinates::distance(const Coordinates& lhs, const Coordinates& rhs) {
+    return std::sqrt(std::pow((int)lhs.positionX_ - (int)rhs.positionX_, 2) + std::pow((int)lhs.positionY_ - (int)rhs.positionY_, 2));
 }
 
 Island::Island(const Island::Coordinates& position)
